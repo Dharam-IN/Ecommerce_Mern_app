59cfa465e03f4d6aa1ce407a90136df958ef35ee
dharamraj-dev <dharamrajyadav6312@gmail.com>
2023-12-15 18:22:10 +0530

    Create server and install package

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+// rest object
+const app = express()
+
+// config
+dotenv.config()
+
+// rest api
+app.get("/", (req, res)=>{
+    res.send({
+        message: "Welcome to ecommerse app"
+    })
+})
+
+const PoRT = process.env.PORT
+
+app.listen(PoRT, ()=>{
+    console.log(`Server running on ${PoRT}`)
+})
