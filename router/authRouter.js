0d1dacf70c3f2b0e8025133b66834a6e04b133d3
dharamraj-dev <dharamrajyadav6312@gmail.com>
2023-12-18 18:01:40 +0530

    Login data check and JWT Token generate

--- router/authRouter.js
+++ router/authRouter.js
@@ -1,12 +1,15 @@
 import express from 'express'
-import {registerController} from '../controllers/authController.js'
+import {registerController, loginController } from '../controllers/authController.js'
 
 // router object
 const router = express.Router()
 
 // routing
 // Register || Method POST
+router.post('/register', registerController);
 
-router.post('/register', registerController)
+// Login || POST
+router.post('/login', loginController);
+
 
 export default router;
