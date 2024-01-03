e7b3e93452e90c512fca430fb0fc6ea4de23746d
dharamraj-dev <dharamrajyadav6312@gmail.com>
2024-01-03 17:32:27 +0530

    Private Route Created

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import Policy from './components/pages/Policy';
 import Pagenotfound from './components/pages/Pagenotfound';
 import Register from './components/pages/Auth/Register';
 import Login from './components/pages/Auth/Login';
+import Dashboard from './components/pages/user/Dashboard';
+import PrivateRoute from './components/Routes/Private';
 
 
 function App() {
@@ -13,6 +15,9 @@ function App() {
     <>
       <Routes>
         <Route path='/' element={<HomePage/>}/>
+        <Route path='/dashboard' element={<PrivateRoute/>}>
+          <Route path='' element={<Dashboard/>}/>
+        </Route>
         <Route path='/register' element={<Register/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/about' element={<About/>}/>
---
+++ client/src/components/pages/user/Dashboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../../layout/Layout";
+
+const Dashboard = ()=>{
+    return(
+        <>
+            <Layout title={"Dashboard - Aadi Cart"}>
+                Dashboard
+            </Layout>
+        </>
+    )
+}
+
+export default Dashboard;
